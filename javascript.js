4ba59b48ab3cbf1c8527a5cc9b2c70031ff61818
Sloane Tribble <sloanetribble@gmail.com>
2026-07-21 16:03:33 -0700

    Add instructions for JS functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+/* book constructor;
+    attributes:
+        - unique ID
+        - author
+        - title
+        - number of pages
+        - read (T/F)
+*/
+
+// function to create a Book object from arguments and store it in an array
+
+// function to iterate through the Books array and display each on the page
+
+// function to allow users to add a new book to Books
+
+// function to allow users to remove a book from Books 
+
+// function to change a book's "read" status
